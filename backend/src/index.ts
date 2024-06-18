9383fe4cf62a1267cae8e8014d704f0d856163f1
Veighnsche <vincepaul.liem@gmail.com>
2024-06-18 23:32:57 +0200

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (_, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
